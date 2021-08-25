ce92edbb2bb2114155c1b9f0b5e802f506288c1e
ehylin <ehylinolezjua@gmail.com>
2021-08-24 23:07:38 -0400

    desafio vue props

---
+++ src/components/Todo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Crea una nueva tarea</h1>
+
+    <TodoAdd @nueva-actividad="agregarNuevaActividad" />
+
+    <TodoList
+      :actividades="actividades"
+      @eliminar-actividad="eliminarActividad"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from "./TodoList.vue";
+import TodoAdd from "./TodoAdd.vue";
+
+export default {
+  name: "Todo",
+  components: {
+    TodoList,
+    TodoAdd,
+  },
+  data: () => ({
+    actividades: [],
+  }),
+  methods: {
+    agregarNuevaActividad(nuevaActividad) {
+      this.actividades.push(nuevaActividad);
+    },
+    eliminarActividad(indiceActividad) {
+      this.actividades.splice(indiceActividad, 1);
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Lista</h1>
+
+    <ul>
+      <li v-for="(actividad, $index) in actividades" :key="$index">
+        {{ actividad }}
+        <button @click="$emit('eliminar-actividad', $index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    actividades: { type: Array, required: true },
+  },
+};
+</script>
+
+<style></style>
